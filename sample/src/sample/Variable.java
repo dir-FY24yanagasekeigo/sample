12dc026d1a2e9d03a74054d45004492557aef8a4
dir-FY24yanagasekeigo <keigo.yanagase.k338@dirbato.co.jp>
2024-04-05 15:15:24 +0900

    レッスン２_チャプター４

--- sample/src/sample/Variable.java
+++ sample/src/sample/Variable.java
@@ -3,7 +3,14 @@ package sample;
 public class Variable {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        var number1 = 1;
+        System.out.println(number1);
+        
+        
+        String str1;
+        str1 = "abc";
+        System.out.println(str1);
+       
 
     }
 
