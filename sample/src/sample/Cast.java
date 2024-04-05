151b3a2295e39cbf9580a3077f03fbcebb11da6e
dir-FY24yanagasekeigo <keigo.yanagase.k338@dirbato.co.jp>
2024-04-05 16:18:08 +0900

    レッスン２_チャプター5終了

---
+++ sample/src/sample/Cast.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Cast {
+
+    public static void main(String[] args) {
+        var a = 215;
+        var b = 207;
+        
+        
+        System.out.println( a / b );
+        System.out.println( (double)a / b );
+        System.out.println( (double)(a / b) );
+        
+
+    }
+
+}
---
+++ sample/src/sample/Compare.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Compare {
+
+    public static void main(String[] args) {
+        var a = 215;
+        var b = 207;
+        
+        
+        System.out.println("a > b は" + (a > b));
+        System.out.println("a < b は" + (a < b));
+        
+
+    }
+
+}
